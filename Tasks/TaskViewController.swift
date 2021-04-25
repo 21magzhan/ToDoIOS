29ff82def3bb74120c3077240b91e2cc1e96ed8c
Magzhan <magzhanomirtay@icloud.com>
2021-04-25 18:44:17 +0600

    first task done

---
+++ Tasks/TaskViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TaskViewController.swift
+//  Tasks
+//
+//  Created by User on 22.04.2021.
+//
+
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    @objc func deleteTask(){
+//
+//        let newCount = count - 1
+//
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+        
+    }
+
+
+
+}
+
